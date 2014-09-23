d7fb9f4960787383ec69f0655b8a19948da6fe7f
Utsav Patel <utsav.sb.patel@gmail.com>
2014-09-23 15:23:25 -0400

    Implemented exception.

--- edu/buffalo/cse/irf14/document/ParserException.java
+++ edu/buffalo/cse/irf14/document/ParserException.java
@@ -8,7 +8,10 @@ package edu.buffalo.cse.irf14.document;
  * Generic wrapper exception class for parsing exceptions
  */
 public class ParserException extends Exception {
-
+	public ParserException() {super();}
+	public ParserException(String s){super(s);}
+	public ParserException(String s, Throwable c) {super(s,c);}
+	public ParserException(Throwable c) {super(c);}
 	/**
 	 * 
 	 */
