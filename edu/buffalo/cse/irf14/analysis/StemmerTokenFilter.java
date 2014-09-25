7bc0939f08a77bb849aa7d49e88ba93a62835b96
aayushuppal <aayushup@buffalo.edu>
2014-09-25 04:21:30 -0400

    PorterStemmer implementation

--- edu/buffalo/cse/irf14/analysis/StemmerTokenFilter.java
+++ edu/buffalo/cse/irf14/analysis/StemmerTokenFilter.java
@@ -3,15 +3,35 @@ package edu.buffalo.cse.irf14.analysis;
  * A stemmer that replaces words with their stemmed versions.
  */
 public class StemmerTokenFilter extends TokenFilter{
-
+	TokenStream localstream;	
+	PorterStemmer psobj = new PorterStemmer();
+	
 	public StemmerTokenFilter(TokenStream stream) {
 		super(stream);
-		// TODO Auto-generated constructor stub
+		localstream=stream;
+		filter();
+		
 	}
 
 	@Override
 	public void filter() {
-		// TODO Auto-generated method stub
+		localstream.reset();
+		Token token;
+		
+		try {
+			while(increment()){
+			token=localstream.next();
+			
+			char tokenchar[] = token.getTermBuffer();
+			psobj.add(tokenchar, tokenchar.length);
+			psobj.stem();
+			String text = psobj.toString();
+			token.setTermText(text);
+			}
+		}
+		catch (TokenizerException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
