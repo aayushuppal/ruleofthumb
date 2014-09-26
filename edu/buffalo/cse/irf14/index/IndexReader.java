25956a6fa0f171623f90f78b8c90577523c27e2e
Utsav Patel <utsav.sb.patel@gmail.com>
2014-09-26 18:45:34 -0400

    Index Reader, Writer implemented. (Query remains)

--- edu/buffalo/cse/irf14/index/IndexReader.java
+++ edu/buffalo/cse/irf14/index/IndexReader.java
@@ -3,14 +3,22 @@
  */
 package edu.buffalo.cse.irf14.index;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * @author nikhillo
  * Class that emulates reading data back from a written index
  */
 public class IndexReader {
+	TreeMap<String,Integer> sorted_map;
 	/**
 	 * Default constructor
 	 * @param indexDir : The root directory from which the index is to be read.
@@ -19,6 +27,7 @@ public class IndexReader {
 	 * @param type The {@link IndexType} to read from
 	 */
 	public IndexReader(String indexDir, IndexType type) {
+		
 		//TODO
 	}
 	
@@ -29,7 +38,7 @@ public class IndexReader {
 	 */
 	public int getTotalKeyTerms() {
 		//TODO : YOU MUST IMPLEMENT THIS
-		return -1;
+		return IndexWriter.termCounter;
 	}
 	
 	/**
@@ -39,7 +48,7 @@ public class IndexReader {
 	 */
 	public int getTotalValueTerms() {
 		//TODO: YOU MUST IMPLEMENT THIS
-		return -1;
+		return IndexWriter.docCounter;
 	}
 	
 	/**
@@ -52,7 +61,119 @@ public class IndexReader {
 	 */
 	public Map<String, Integer> getPostings(String term) {
 		//TODO:YOU MUST IMPLEMENT THIS
-		return null;
+		int freq;
+		String textLowerCase=term.toLowerCase();
+		HashMap<String,ArrayList<Integer>> map = null;
+		textLowerCase=term.toLowerCase();
+		
+		if(textLowerCase.equals("a")) map=IndexWriter.aa_an;
+		else if(textLowerCase.charAt(0)=='a'){
+			if((textLowerCase.charAt(1)>='a' && textLowerCase.charAt(1)<='n'))
+			{
+				map=IndexWriter.aa_an;
+			}
+			else{
+				map=IndexWriter.ao_az;
+			}
+			
+		}
+	
+		if(textLowerCase.equals("c")) map=IndexWriter.ca_cj;
+		else if(textLowerCase.charAt(0)=='c'){
+			if(textLowerCase.charAt(1)>='a' && textLowerCase.charAt(1)<='j'){
+				map=IndexWriter.ca_cj;
+			}
+			else{
+				map=IndexWriter.ck_cz;
+			}
+		}
+		if(textLowerCase.equals("s")) map=IndexWriter.sa_si;
+		else if(textLowerCase.charAt(0)=='s'){
+			if(textLowerCase.charAt(1)>='a' && textLowerCase.charAt(1)<='i'){
+				map=IndexWriter.sa_si;
+			}
+			else{
+				map=IndexWriter.sj_sz;
+			}
+		}
+		
+		else if(textLowerCase.charAt(0)=='b'){
+			map=IndexWriter.b;
+		}
+		else if(textLowerCase.charAt(0)=='d'){
+			map=IndexWriter.d;
+		}
+		else if(textLowerCase.charAt(0)=='e'){
+			map=IndexWriter.e;
+		}
+		else if(textLowerCase.charAt(0)=='f'){
+			map=IndexWriter.f;
+		}
+		else if(textLowerCase.charAt(0)=='g'){
+			map=IndexWriter.g;
+		}
+		else if(textLowerCase.charAt(0)=='h'){
+			map=IndexWriter.h;
+		}
+		else if(textLowerCase.charAt(0)=='i'){
+			map=IndexWriter.i;
+		}
+		else if(textLowerCase.charAt(0)=='j'){
+			map=IndexWriter.j;
+		}
+		else if(textLowerCase.charAt(0)=='k'){
+			map=IndexWriter.k;
+		}
+		else if(textLowerCase.charAt(0)=='l'){
+			map=IndexWriter.l;
+		}
+		else if(textLowerCase.charAt(0)=='m'){
+			map=IndexWriter.m;
+		}
+		else if(textLowerCase.charAt(0)=='n'){
+			map=IndexWriter.n;
+		}
+		else if(textLowerCase.charAt(0)=='o'){
+			map=IndexWriter.o;
+		}
+		else if(textLowerCase.charAt(0)=='p'){
+			map=IndexWriter.p;
+		}
+		else if(textLowerCase.charAt(0)=='q'){
+			map=IndexWriter.q;
+		}
+		else if(textLowerCase.charAt(0)=='r'){
+			map=IndexWriter.r;
+		}
+		else if(textLowerCase.charAt(0)=='t'){
+			map=IndexWriter.t;
+		}
+		else if(textLowerCase.charAt(0)=='u'){
+			map=IndexWriter.u;
+		}
+		else if(textLowerCase.charAt(0)=='v' ||textLowerCase.charAt(0)=='w' ||textLowerCase.charAt(0)=='x' ||textLowerCase.charAt(0)=='y' ||textLowerCase.charAt(0)=='z'){
+			map=IndexWriter.vwxyz;
+		}
+		else{
+			map=IndexWriter.symbol;
+		}
+		
+		ArrayList<Integer> postList = new ArrayList<Integer>();
+		HashMap<String, Integer> map2=new HashMap<String,Integer>();
+		
+		if(map.containsKey(term)){
+			postList=map.get(term);
+		for(int i=0;i+2<=postList.size();i=i+2){
+			String docID= IndexWriter.revDocMap.get(postList.get(i));
+//			System.out.println(i);
+			int freq1 = postList.get(i+1);
+			map2.put(docID, freq1);
+		}
+		}
+		else{
+			map2=null;
+		}
+		return map2;
 	}
 	
 	/**
@@ -63,10 +184,36 @@ public class IndexReader {
 	 * null for invalid k values
 	 */
 	public List<String> getTopK(int k) {
+		if(k>0)
+		{
 		//TODO YOU MUST IMPLEMENT THIS
-		return null;
+		Map<String,Integer> sortMap = new HashMap<String,Integer>();
+		ValueComparator bvc =  new ValueComparator(sortMap);
+		sorted_map = new TreeMap<String,Integer>(bvc);
+		HashMap[] list={IndexWriter.aa_an, IndexWriter.ao_az,IndexWriter.ca_cj,IndexWriter.ck_cz,IndexWriter.sa_si,IndexWriter.sj_sz,IndexWriter.b,IndexWriter.d,IndexWriter.e,IndexWriter.f,IndexWriter.g,IndexWriter.h,IndexWriter.i,IndexWriter.j,IndexWriter.k,IndexWriter.l,IndexWriter.m,IndexWriter.n,IndexWriter.o,IndexWriter.p,IndexWriter.q,IndexWriter.r,IndexWriter.t,IndexWriter.u,IndexWriter.vwxyz};
+		for(HashMap<String,ArrayList<Integer>> map:list){
+			for(String ls:map.keySet()){
+				int sum=0;
+				for(int i=0;i+1<map.get(ls).size();i=i+2){
+					sum=sum+map.get(ls).get(i+1);
+				}
+//				System.out.println("lol");
+				sortMap.put(ls, sum);
+			}
+		}
+		sorted_map.putAll(sortMap);
+		int i=0;
+		List<String> list1=new ArrayList<String>();
+		for(String k2:sorted_map.keySet()){
+			i++;
+			if(i>k) break;
+			list1.add(k2);
+			
+		}
+		return list1;
 	}
-	
+		else return null;
+	}
 	/**
 	 * Method to implement a simple boolean AND query on the given index
 	 * @param terms The ordered set of terms to AND, similar to getPostings()
@@ -79,6 +226,23 @@ public class IndexReader {
 	 */
 	public Map<String, Integer> query(String...terms) {
 		//TODO : BONUS ONLY
+		
 		return null;
 	}
+}
+class ValueComparator implements Comparator<String> {
+
+    Map<String, Integer> base;
+    public ValueComparator(Map<String, Integer> base) {
+        this.base = base;
+    }
+
+    // Note: this comparator imposes orderings that are inconsistent with equals.    
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
+    }
 }
--- edu/buffalo/cse/irf14/index/IndexWriter.java
+++ edu/buffalo/cse/irf14/index/IndexWriter.java
@@ -20,7 +20,35 @@ public class IndexWriter {
 	
 	String indexDir;
 	static HashMap<String,Integer> docMap = new HashMap<String,Integer>();
-	static HashMap<String,ArrayList<Integer>> indexMap = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<Integer,String> revDocMap = new HashMap<Integer,String>();
+	static HashMap<String,ArrayList<Integer>> indexMap=null;
+	static HashMap<String,ArrayList<Integer>> aa_an = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> ao_az = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> ca_cj = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> ck_cz = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> sa_si = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> sj_sz = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> b = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> d = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> e = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> f = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> g = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> h = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> i = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> j = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> k = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> l = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> m = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> n = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> o = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> p = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> q = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> r = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> t = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> u = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> vwxyz = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,ArrayList<Integer>> symbol = new HashMap<String,ArrayList<Integer>>();
+	static HashMap<String,String> list = new HashMap<String,String>();
 	static int docCounter=0;
 	static String docNameKey;
 	public static int termCounter;
@@ -92,7 +120,8 @@ public class IndexWriter {
 		if(docMap.get(docNameKey)==null){
 			docCounter++;
 			docMap.put(docNameKey, docCounter);
-			
+			revDocMap.put(docCounter,docNameKey);
+//			System.out.println(docNameKey);
 			return docCounter;			//which is docID
 		}
 		else return docMap.get(docNameKey);
@@ -100,20 +129,125 @@ public class IndexWriter {
 	
 	public void index(TokenStream s){
 		String text;
+		String textLowerCase;
 		while(s.hasNext()){
 			text=s.next().getTermText();
+			textLowerCase=text.toLowerCase();
+			if(textLowerCase.equals("a")) indexMap=aa_an;
+			else if(textLowerCase.charAt(0)=='a'){
+				if((textLowerCase.charAt(1)>='a' && textLowerCase.charAt(1)<='n'))
+				{
+					indexMap=aa_an;
+				}
+				else{
+					indexMap=ao_az;
+				}
+				
+			}
+		
+			if(textLowerCase.equals("c")) indexMap=ca_cj;
+			else if(textLowerCase.charAt(0)=='c'){
+				if(textLowerCase.charAt(1)>='a' && textLowerCase.charAt(1)<='j'){
+					indexMap=ca_cj;
+				}
+				else{
+					indexMap=ck_cz;
+				}
+			}
+			if(textLowerCase.equals("s")) indexMap=sa_si;
+			else if(textLowerCase.charAt(0)=='s'){
+				if(textLowerCase.charAt(1)>='a' && textLowerCase.charAt(1)<='i'){
+					indexMap=sa_si;
+				}
+				else{
+					indexMap=sj_sz;
+				}
+			}
+			
+			else if(textLowerCase.charAt(0)=='b'){
+				indexMap=b;
+			}
+			else if(textLowerCase.charAt(0)=='d'){
+				indexMap=d;
+			}
+			else if(textLowerCase.charAt(0)=='e'){
+				indexMap=e;
+			}
+			else if(textLowerCase.charAt(0)=='f'){
+				indexMap=f;
+			}
+			else if(textLowerCase.charAt(0)=='g'){
+				indexMap=g;
+			}
+			else if(textLowerCase.charAt(0)=='h'){
+				indexMap=h;
+			}
+			else if(textLowerCase.charAt(0)=='i'){
+				indexMap=i;
+			}
+			else if(textLowerCase.charAt(0)=='j'){
+				indexMap=j;
+			}
+			else if(textLowerCase.charAt(0)=='k'){
+				indexMap=k;
+			}
+			else if(textLowerCase.charAt(0)=='l'){
+				indexMap=l;
+			}
+			else if(textLowerCase.charAt(0)=='m'){
+				indexMap=m;
+			}
+			else if(textLowerCase.charAt(0)=='n'){
+				indexMap=n;
+			}
+			else if(textLowerCase.charAt(0)=='o'){
+				indexMap=o;
+			}
+			else if(textLowerCase.charAt(0)=='p'){
+				indexMap=p;
+			}
+			else if(textLowerCase.charAt(0)=='q'){
+				indexMap=q;
+			}
+			else if(textLowerCase.charAt(0)=='r'){
+				indexMap=r;
+			}
+			else if(textLowerCase.charAt(0)=='t'){
+				indexMap=t;
+			}
+			else if(textLowerCase.charAt(0)=='u'){
+				indexMap=u;
+			}
+			else if(textLowerCase.charAt(0)=='v' ||textLowerCase.charAt(0)=='w' ||textLowerCase.charAt(0)=='x' ||textLowerCase.charAt(0)=='y' ||textLowerCase.charAt(0)=='z'){
+				indexMap=vwxyz;
+			}
+			else{
+				indexMap=symbol;
+			}
+			
+			
 			if(!indexMap.containsKey(text)){
 				ArrayList<Integer> postingList=new ArrayList<Integer>();
 				postingList.add(docCounter);
+				postingList.add(1);
 				indexMap.put(text, postingList);
 				termCounter++;
 			}
 			else{
 				ArrayList<Integer> postingList=indexMap.get(text);
 				if(!postingList.contains(docCounter))
+					{
 					postingList.add(docCounter);
+					postingList.add(1);
+					}
+				else{
+					int freq=postingList.get((postingList.indexOf(docCounter)+1))+1;
+					postingList.add(postingList.indexOf(docCounter)+1,freq);
+				}
 				indexMap.put(text, postingList);
+			
 			}
+//			System.out.println(text);
 		}
 	}
 	
