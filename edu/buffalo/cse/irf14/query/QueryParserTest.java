ae67d71e86cb15a0a6830bb93adc852151b54832
Utsav Patel <utsav.sb.patel@gmail.com>
2014-10-19 18:37:46 -0400

    temp query parser

--- edu/buffalo/cse/irf14/query/QueryParserTest.java
+++ edu/buffalo/cse/irf14/query/QueryParserTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.TreeMap;
+
+import edu.buffalo.cse.irf14.index.IndexReader;
 /////////////////////////////////
 // infinite loop query tester////
 /////////////////////////////////
@@ -18,20 +20,25 @@ public class QueryParserTest {
 		System.out.println("Enter your Query:");
 		String s = terminalInput.nextLine();
 		Query q =QueryParser.parse(s, "OR");
-		String s2=q.toString();
+//		System.out.println(q.toString());
+//		q.getSplitQuery();
+//		IndexReader indexer =IndexFactory.getIndexReader("C:\\Users\\Festy\\Desktop\\IR Slides\\sample");
+		IndexSearcher s1 = new IndexSearcher("C:\\Users\\Festy\\Desktop\\IR Slides\\sample");
+		s1.search(q);
+//		String s2=q.toString();
 		
-		System.out.println(s2);
-		
-		/*Assume that the query to be scored is (A AND B) OR (C AND D)
-		 * The result returned by an IndexSearcher method will be a HashMap with
-		 * "Term">"Doc, FreqofTerm" as a pair------------------------------------*/
-		TreeMap<String,String[]> result= new TreeMap<String,String[]> ();
-		result.put("pull", new String[]{"0000005","2","0000007","5","0000002","1"});
-		result.put("push", new String[]{"0000007","1","0000005","2","0000006","1"});
-		result.put("world", new String[]{"000000","1","401001","2","601001","6","101001","2","801001","3"});
-		result.put("crop", new String[]{"101001","3","201001","4"});
-//		//TFScorer scr = new TFScorer(result,q);
-		OKScorer scr = new OKScorer(result,q);
+//		System.out.println(s2);
+//		
+//		/*Assume that the query to be scored is (A AND B) OR (C AND D)
+//		 * The result returned by an IndexSearcher method will be a HashMap with
+//		 * "Term">"Doc, FreqofTerm" as a pair------------------------------------*/
+//		TreeMap<String,String[]> result= new TreeMap<String,String[]> ();
+//		result.put("pull", new String[]{"0000005","2","0000007","5","0000002","1"});
+//		result.put("push", new String[]{"0000007","1","0000005","2","0000006","1"});
+//		result.put("world", new String[]{"000000","1","401001","2","601001","6","101001","2","801001","3"});
+//		result.put("crop", new String[]{"101001","3","201001","4"});
+////		//TFScorer scr = new TFScorer(result,q);
+//		TFScorer scr = new TFScorer(result,q);
 		}
 		
 	}
